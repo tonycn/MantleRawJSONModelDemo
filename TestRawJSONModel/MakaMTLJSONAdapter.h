203931e31e6df18ad532d4212b334de639b1fe32
jianjun <jianjun@maka.im>
2020-02-06 20:42:12 +0800

    first commit

---
+++ TestRawJSONModel/MakaMTLJSONAdapter.h
@@ -0,0 +1,17 @@
+//
+//  MakaMTLJSONAdapter.h
+//  TestRawJSONModel
+//
+//  Created by jianjun on 2020-02-06.
+//  Copyright © 2020 Jianjun. All rights reserved.
+//
+
+#import "MTLJSONAdapter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MakaMTLJSONAdapter : MTLJSONAdapter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestRawJSONModel/MakaMTLJSONAdapter.m
@@ -0,0 +1,13 @@
+//
+//  MakaMTLJSONAdapter.m
+//  TestRawJSONModel
+//
+//  Created by jianjun on 2020-02-06.
+//  Copyright © 2020 Jianjun. All rights reserved.
+//
+
+#import "MakaMTLJSONAdapter.h"
+
+@implementation MakaMTLJSONAdapter
+
+@end
